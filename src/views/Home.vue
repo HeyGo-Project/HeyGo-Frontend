207f4c4775743a4fe8c392404de49a9407739735
Alisher <alisher.zharm@gmail.com>
2021-02-14 23:50:19 +0600

    Added home page with toolbar

---
+++ src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+      <MainPage/>
+
+      Products  
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import MainPage from "@/components/MainPage.vue";
+
+export default {
+  name: "home",
+  components: {
+    MainPage
+  }
+};
+</script>
+
+<style>
+
+</style>
+
