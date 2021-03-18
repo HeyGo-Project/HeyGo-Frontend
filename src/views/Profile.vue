2468ac24a9218364681e583424e87bc56db89e34
Alisher <alisher.zharm@gmail.com>
2021-03-18 17:32:21 +0600

    complete change of profile page

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,117 +1,196 @@
 <template>
-  <div>
-    <div class="container bootstrap snippets bootdey">
-      <div class="row">
-        <ul class="user-profile-card col-md-3">
-          <div style=":background-image: url(imgUrl) ">
-            <img
-              src="https://i.kinja-img.com/gawker-media/image/upload/gd8ljenaeahpn0wslmlz.jpg"
-              class="image--cover"
-              alt=""
-            />
+  <div class="main">
+    <div class="container" style="margin-top: 100px">
+      <div class="profile-header">
+        <div class="profile-img ">
+          <img :src="imgUrl" alt="" width="200px" />
+        </div>
+        <div class="profile-nav-info">
+          <h3 class="user-name">Alisher</h3>
+          <div class="address">
+            <p class="state">Almaty</p>
+            <span class="country">Kazakhstan</span>
           </div>
-          <h5>Alisher Zharmukhambetov</h5>
-          <p>23087@iitu.edu.kz</p>
-          <p></p>
-          <div
-            class="user-nav nav flex-column nav-pills"
-            id="v-pills-tab"
-            role="tablist"
-            aria-orientation="vertical"
-          >
-            <a
-              class="nav-link active"
-              id="v-pills-home-tab"
-              data-toggle="pill"
-              href="#v-pills-home"
-              role="tab"
-              aria-controls="v-pills-home"
-              aria-selected="true"
-            >Profile</a
-            >
-            <a
-              class="nav-link"
-              id="v-pills-profile-tab"
-              data-toggle="pill"
-              href="#v-pills-profile"
-              role="tab"
-              aria-controls="v-pills-profile"
-              aria-selected="false"
-            >Edit</a
-            >
-          </div>
-        </ul>
-
-        <div class="tab-content  col-md-2">
-              <div
-                class="tab-pane fade show active"
-                id="v-pills-home"
-                role="tabpanel"
-                aria-labelledby="v-pills-home-tab"
-              >
-              <h1>Profile</h1>
-
-              </div>
-              <div
-                class="tab-pane fade"
-                id="v-pills-profile"
-                role="tabpanel"
-                aria-labelledby="v-pills-profile-tab"
-              >
-                luk
-              </div>
-            </div>
-
 
         </div>
 
+      </div>
+
+      <div class="main-bd">
+        <div class="left-side">
+
+          <div class="profile-side">
+            <p class="mobile-number">
+              <i class="fa fa-phone"></i>{{ phoneNumber }}
+            </p>
+            <div class="user-email">
+              <i class="fa fa-envelope"></i>{{userEmail}}
+            </div>
+            <div class="user-bio">
+              <p class="bio">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+                eiusmod tempor incididunt.
+              </p>
+            </div>
+            <div class="profile-btn">
+              <button class="createbtn">
+                <i class="fa fa-comment"></i>Leave a comment
+              </button>
+              <button class="createbtn">
+                <i class="fa fa-comment"></i>Leave a comment
+              </button>
+            </div>
+            <div class="user-rating">
+              <h3 class="rating">4.5</h3>
+              <div class="rate">
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+              </div>
+              <span class="no-user">
+              <span>123</span>
+              &nbsp;&nbsp; reviews
+            </span>
+            </div>
+          </div>
+
+        </div>
+      </div>
+      <div class="right-side">
+        <div class="nav">
+          <ul>
+            <li class="user-review active"></li>
+            <li class="user-setting"></li>
+          </ul>
+        </div>
+        <div class="profile-body">
+          <div class="profile-review">
+            <h1>Profile reviews</h1>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+          <div class="profile-settings">
+            <h1>Profile settings</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est
+              exercitationem maxime molestiae nemo qui. Distinctio facere fugit
+              incidunt iste officia? Adipisci consequuntur dignissimos
+              doloremque iusto nemo quia? Aliquam perferendis, repellendus.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import "jquery";
 export default {
   name: "Profile",
   data() {
     return {
-      userFirstName: "",
-      userLastName: "",
-      userEmail: "",
+      firstName: "Alisher",
+      lastName: "Zharmukhambetov",
+      userEmail: "alisher.zharm@gmail.com",
       imgUrl:
-        "https://i.kinja-img.com/gawker-media/image/upload/gd8ljenaeahpn0wslmlz.jpg"
+        "https://i.kinja-img.com/gawker-media/image/upload/gd8ljenaeahpn0wslmlz.jpg",
+      phoneNumber: "+77072109620"
     };
   },
   methods() {}
 };
 </script>
 
+<style scoped lang="scss">
 
-<style scoped lang="scss" > .user-nav {
-  margin-bottom: 15px;
+.main {
+  width: 100%;
+  text-align: left;
+  overflow-x:hidden;
+  font-family: "Nunito", sans-serif;
+  background: #e9e9e9;
 }
-
-.tab-content {
-  alignment: right;
-  margin-left: 50px;
+.profile-img{
+  float: left;
+  width: 340px;
+  height: 200px;
 }
-
-.user-profile-card {
-  border: 2px solid #f2f2f2;
-  border-radius: 5px;
-}
-
-.row {
-
-  margin-top: 90px;
-}
-
-.image--cover {
-  width: 150px;
-  height: 150px;
+.profile-img img{
   border-radius: 50%;
-  margin: 10px;
-  alignment: center;
-  object-fit: cover;
-  object-position: center right;
+  height: 230px;
+  width: 230px;
+  border: 4px solid white;
+  position: absolute;
+  left: 50px;
+  box-shadow: 0 0 10px rgba(0,0,0,.2); /* Параметры тени */
+  top:20px;
+  z-index: 5;
+  background: white;
+}
+.profile-header{
+  background: #ffffff;
+  width: 100%;
+  display: flex;
+  height: 190px;
+  position: relative;
+  box-shadow: 0px 3px 4px rgba(0,0,0,.2); /* Параметры тени */
+}
+.profile-nav-info{
+  float:left;
+  flex-direction: column;
+  justify-content: center ;
+  padding-top: 60px;
+}
+.profile-nav-info h3{
+ font-variant: small-caps;
+  font-size: 2rem;
+  font-family: sans-serif;
+  font-weight: bold;
+}
+.profile-nav-info .address{
+  display: flex;
+  font-weight: bold;
+  color: #777;
+}
+.profile-nav-info .address p{
+  margin-right: 5px;
+}
+.profile-side{
+  width: 300px;
+  background: white;
+  box-shadow: 0px 3px 5px rgba(0,0,0,.2);
+  padding: 90px 30px 20px;
+  font-family: "Bree Serif",serif;
+  margin-left: 10px;
+  z-index: 99;
+}
+.profile-side p{
+  margin-bottom: 7px;
+  color: #333 ;
+  font-size: 14px;
+}
+.profile-side i{
+   color: #17A2B8;
+}
+.mobile-number i{
+  transform: rotateY(180deg);
+}
+.profile-btn{
+  display: flex;
+}
+.profile-btn i{
+  color: black;
+  margin-right: 5px;
+}
+.createbtn{
+  border:0;
+  padding: 5px;
+  width: 100%;
+  border-radius: 3px;
+  background: #17A2B8;
+  font-family: "Bree Serif", sans-serif;
+  margin: 5px 2px;
 }
 </style>
