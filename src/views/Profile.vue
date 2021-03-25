a269130ebfa3d6d58c9b9d95c42bb90f78b28400
Alisher <alisher.zharm@gmail.com>
2021-03-25 23:35:31 +0600

    html fix

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,83 +1,73 @@
 <template>
   <div class="main">
     <div class="container" style="margin-top: 100px">
-      <div class="profile-header">
-        <div class="profile-img ">
-          <img :src="imgUrl" alt="" width="200px" />
-        </div>
-        <div class="profile-nav-info">
-          <h3 class="user-name">Alisher</h3>
-          <div class="address">
-            <p class="state">Almaty</p>
-            <span class="country">Kazakhstan</span>
+      <div class="container">
+        <div class="profile-header">
+          <div class="profile-img">
+            <img
+              src="https://i.kinja-img.com/gawker-media/image/upload/gd8ljenaeahpn0wslmlz.jpg"
+              alt=""
+              width="200"
+            />
           </div>
-
-        </div>
-
-      </div>
-
-      <div class="main-bd">
-        <div class="left-side">
-
-          <div class="profile-side">
-            <p class="mobile-number">
-              <i class="fa fa-phone"></i>{{ phoneNumber }}
-            </p>
-            <div class="user-email">
-              <i class="fa fa-envelope"></i>{{userEmail}}
+          <div class="profile-nav-info">
+            <h3 class="user-name">Alisher Zharmukhambetov</h3>
+            <div class="addres">
+              <p class="state">Almaty</p>
+              <span class="country">Kazakhstan</span>
             </div>
-            <div class="user-bio">
-              <p class="bio">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-                eiusmod tempor incididunt.
+          </div>
+          <div class="main-bd">
+            <div class="left-side">
+              <p class="mobile-no"><i class="fa fa-phone"></i>+77072109620</p>
+              <p  class="user-email">
+                <i class="fa fa-envelope">alisher.zharm@gmail.com</i>
               </p>
-            </div>
-            <div class="profile-btn">
-              <button class="createbtn">
-                <i class="fa fa-comment"></i>Leave a comment
-              </button>
-              <button class="createbtn">
-                <i class="fa fa-comment"></i>Leave a comment
-              </button>
-            </div>
-            <div class="user-rating">
-              <h3 class="rating">4.5</h3>
-              <div class="rate">
-                <i class="fa fa-star"></i>
-                <i class="fa fa-star"></i>
-                <i class="fa fa-star"></i>
-                <i class="fa fa-star"></i>
-                <i class="fa fa-star"></i>
+              <div class="user-bio">
+                <p class="bio">Lorem ipsum dolor sit, amet consectetur
+                  adipisicing elit. Iste, impedit
+                  odit, assumenda mollitia,
+                  cupiditate magni eveniet ipsam voluptatibus dolorem
+                  in dolore exercitationem quasi molestias
+                  illum asperiores tenetur cum neque soluta?</p>
               </div>
-              <span class="no-user">
-              <span>123</span>
-              &nbsp;&nbsp; reviews
-            </span>
+              <div class="profile-btn">
+                <button class="chatbtn"><i class="fa fa-comment"></i>Feedback</button>
+              </div>
+              <div class="user-rating">
+                <h3 class="rating">4.5</h3>
+                <div class="stars">
+                  <i class="fa fa-star"></i>
+                  <i class="fa fa-star"></i>
+                  <i class="fa fa-star"></i>
+                  <i class="fa fa-star"></i>
+                  <i class="fa fa-star"></i>
+                </div>
+              </div>
             </div>
           </div>
-
-        </div>
-      </div>
-      <div class="right-side">
-        <div class="nav">
-          <ul>
-            <li class="user-review active"></li>
-            <li class="user-setting"></li>
-          </ul>
-        </div>
-        <div class="profile-body">
-          <div class="profile-review">
-            <h1>Profile reviews</h1>
-            <p>Lorem ipsum dolor sit amet.</p>
-          </div>
-          <div class="profile-settings">
-            <h1>Profile settings</h1>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est
-              exercitationem maxime molestiae nemo qui. Distinctio facere fugit
-              incidunt iste officia? Adipisci consequuntur dignissimos
-              doloremque iusto nemo quia? Aliquam perferendis, repellendus.
-            </p>
+          <div class="right-side">
+            <div class="nav">
+              <ul>
+                <li class="user-review active"></li>
+                <li class="user-setting"></li>
+              </ul>
+            </div>
+            <div class="profile-body">
+              <div class="profile-review">
+                <h1>Profile reviews</h1>
+                <p>Lorem ipsum dolor sit amet.</p>
+              </div>
+              <div class="profile-settings">
+                <h1>Profile settings</h1>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est
+                  exercitationem maxime molestiae nemo qui. Distinctio facere fugit
+                  incidunt iste officia? Adipisci consequuntur dignissimos
+                  doloremque iusto nemo quia? Aliquam perferendis, repellendus.
+                </p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
@@ -104,7 +94,13 @@ export default {
 </script>
 
 <style scoped lang="scss">
-
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@100;200&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Bree+Serif&display=swap');
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: 0;
+}
 .main {
   width: 100%;
   text-align: left;
@@ -129,68 +125,68 @@ export default {
   z-index: 5;
   background: white;
 }
-.profile-header{
-  background: #ffffff;
-  width: 100%;
-  display: flex;
-  height: 190px;
-  position: relative;
-  box-shadow: 0px 3px 4px rgba(0,0,0,.2); /* Параметры тени */
-}
-.profile-nav-info{
-  float:left;
-  flex-direction: column;
-  justify-content: center ;
-  padding-top: 60px;
-}
-.profile-nav-info h3{
- font-variant: small-caps;
-  font-size: 2rem;
-  font-family: sans-serif;
-  font-weight: bold;
-}
-.profile-nav-info .address{
-  display: flex;
-  font-weight: bold;
-  color: #777;
-}
-.profile-nav-info .address p{
-  margin-right: 5px;
-}
-.profile-side{
-  width: 300px;
-  background: white;
-  box-shadow: 0px 3px 5px rgba(0,0,0,.2);
-  padding: 90px 30px 20px;
-  font-family: "Bree Serif",serif;
-  margin-left: 10px;
-  z-index: 99;
-}
-.profile-side p{
-  margin-bottom: 7px;
-  color: #333 ;
-  font-size: 14px;
-}
-.profile-side i{
-   color: #17A2B8;
-}
-.mobile-number i{
-  transform: rotateY(180deg);
-}
-.profile-btn{
-  display: flex;
-}
-.profile-btn i{
-  color: black;
-  margin-right: 5px;
-}
-.createbtn{
-  border:0;
-  padding: 5px;
-  width: 100%;
-  border-radius: 3px;
-  background: #17A2B8;
-  font-family: "Bree Serif", sans-serif;
-  margin: 5px 2px;
-}
+//.profile-header{
+//  background: #ffffff;
+//  width: 100%;
+//  display: flex;
+//  height: 190px;
+//  position: relative;
+//  box-shadow: 0px 3px 4px rgba(0,0,0,.2); /* Параметры тени */
+//}
+//.profile-nav-info{
+//  float:left;
+//  flex-direction: column;
+//  justify-content: center ;
+//  padding-top: 60px;
+//}
+//.profile-nav-info h3{
+// font-variant: small-caps;
+//  font-size: 2rem;
+//  font-family: sans-serif;
+//  font-weight: bold;
+//}
+//.profile-nav-info .address{
+//  display: flex;
+//  font-weight: bold;
+//  color: #777;
+//}
+//.profile-nav-info .address p{
+//  margin-right: 5px;
+//}
+//.profile-side{
+//  width: 300px;
+//  background: white;
+//  box-shadow: 0px 3px 5px rgba(0,0,0,.2);
+//  padding: 90px 30px 20px;
+//  font-family: "Bree Serif",serif;
+//  margin-left: 10px;
+//  z-index: 99;
+//}
+//.profile-side p{
+//  margin-bottom: 7px;
+//  color: #333 ;
+//  font-size: 14px;
+//}
+//.profile-side i{
+//   color: #17A2B8;
+//}
+//.mobile-number i{
+//  transform: rotateY(180deg);
+//}
+//.profile-btn{
+//  display: flex;
+//}
+//.profile-btn i{
+//  color: black;
+//  margin-right: 5px;
+//}
+//.createbtn{
+//  border:0;
+//  padding: 5px;
+//  width: 100%;
+//  border-radius: 3px;
+//  background: #17A2B8;
+//  font-family: "Bree Serif", sans-serif;
+//  margin: 5px 2px;
+//}
 </style>
