59dabba1679a2eff06d53d42faae2318b84cf8f7
Alisher <alisher.zharm@gmail.com>
2021-04-08 23:22:04 +0600

    added axios template and env var

---
+++ src/api/axios.js
@@ -0,0 +1,121 @@
+import axios from 'axios'
+
+const axiosConfig = axios.create({
+    baseURL: process.env.API_URL,
+    headers: {
+        'Content-Type': 'application/json',
+    },
+})
+
+const GET = async (url, params = {}, headers = {}) => {
+    try {
+        const response = await axiosConfig({
+            url,
+            method: 'GET',
+            params,
+            headers,
+        })
+
+        if (response.status >= 200 && response.status < 300) {
+            return {
+                ...response,
+                success: true,
+            }
+        }
+
+        return {
+            ...response,
+            success: false,
+        }
+    } catch (error) {
+        return {
+            ...error?.response,
+            success: false,
+        }
+    }
+}
+
+const POST = async (url, data = {}, headers = {}) => {
+    try {
+        const response = await axiosConfig({
+            url,
+            method: 'POST',
+            data,
+            headers,
+        })
+
+        if (response.status >= 200 && response.status < 300) {
+            return {
+                ...response,
+                success: true,
+            }
+        }
+
+        return {
+            ...response,
+            success: false,
+        }
+    } catch (error) {
+        return {
+            ...error?.response,
+            success: false,
+        }
+    }
+}
+
+const PUT = async (url, data = {}, headers = {}) => {
+    try {
+        const response = await axiosConfig({
+            url,
+            method: 'PUT',
+            data,
+            headers,
+        })
+
+        if (response.status >= 200 && response.status < 300) {
+            return {
+                ...response,
+                success: true,
+            }
+        }
+
+        return {
+            ...response,
+            success: false,
+        }
+    } catch (error) {
+        return {
+            ...error?.response,
+            success: false,
+        }
+    }
+}
+
+const DELETE = async (url, headers = {}) => {
+    try {
+        const response = await axiosConfig({
+            url,
+            method: 'DELETE',
+            headers,
+        })
+
+        if (response.status >= 200 && response.status < 300) {
+            return {
+                ...response,
+                success: true,
+            }
+        }
+
+        return {
+            ...response,
+            success: false,
+        }
+    } catch (error) {
+        return {
+            ...error?.response,
+            success: false,
+        }
+    }
+}
+
+export { GET, POST, PUT, DELETE }
