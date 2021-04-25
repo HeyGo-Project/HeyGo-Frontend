559f94a24a46f81a676c4c1585bc276337ea59c6
L-JAZZ <55015558+L-JAZZ@users.noreply.github.com>
2021-04-25 13:47:47 +0600

    flights init

--- src/router.js
+++ src/router.js
@@ -9,6 +9,7 @@ import SearchHotel from "./views/SearchHotel";
 import Registration from "./components/Auth/Registration";
 import Login from "./components/Auth/Login";
 import Logout from "./components/Auth/Logout";
+import Flights from "./views/Flights";
 Vue.use(Router);
 
 export default new Router({
@@ -59,6 +60,11 @@ export default new Router({
       path: "/hotels/search",
       name: "searchHotel",
       component:SearchHotel
+    },
+    {
+      path: "/flights",
+      name: "flights",
+      component:Flights
     }
   ]
 });
